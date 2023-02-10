600e9dff0eedc5222eb718e8a41d25b17d2f1a4b
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2023-02-10 10:15:55 +0500

    bot

---
+++ bot.py
@@ -0,0 +1,34 @@
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update,Bot
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+    CallbackQueryHandler
+    
+)
+from handlers import (
+    start,
+    prayer
+)
+TOKEN = f'6097820347:AAFk8qxkkUSqSVsuUlBTDEjdzMiWTWpA5HU'
+updater = Updater(TOKEN)
+
+def main() -> None:
+    """
+    Run the bot.
+    """
+    # Create the Updater and pass it your bot's token.
+
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CallbackQueryHandler(prayer, pattern="sam"))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
