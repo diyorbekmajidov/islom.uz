471a5b9f60561e6f8d4b6a4677971ab7ca88c763
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2023-02-11 17:34:10 +0500

    pattern

--- handlers.py
+++ handlers.py
@@ -17,6 +17,7 @@ from telegram.ext import (
     Dispatcher
 )
 import requests
+from islom import prayer
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -24,40 +25,22 @@ def start(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat.id
     first_name = update.message.from_user.first_name
     inlinekeyborad = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Qoraqalpog'iston", callback_data = "Karakalpakstan")],
-        [InlineKeyboardButton('Toshkent', callback_data = "Tashkent"), InlineKeyboardButton("Surxondaryo", callback_data = "Surkhandarya")],
-        [InlineKeyboardButton("Andijon", callback_data = 'Andijan'), InlineKeyboardButton("Farg'ona", callback_data = "Ferghana")],
-        [InlineKeyboardButton("Namangan", callback_data = "Namangan"), InlineKeyboardButton("Sirdaryo", callback_data = "Syr Darya")],
-        [InlineKeyboardButton("Smarqand", callback_data = "Smarkand"), InlineKeyboardButton("Qashqadaryo", callback_data = "Kashkadarya")],
-        [InlineKeyboardButton("Buxoro", callback_data="Bukhra"), InlineKeyboardButton("Xorazim", callback_data = "Khorazim")],
-        [InlineKeyboardButton("Navoiy", callback_data = "Navoi"), InlineKeyboardButton("Jizzax", callback_data = "Jizzakh")],
+        [InlineKeyboardButton("Qoraqalpog'iston", callback_data = "👍 Karakalpakstan")],
+        [InlineKeyboardButton('Toshkent', callback_data = "👍 Tashkent"), InlineKeyboardButton("Surxondaryo", callback_data = "👍_Surkhandarya")],
+        [InlineKeyboardButton("Andijon", callback_data = '👍 Andijan'), InlineKeyboardButton("Farg'ona", callback_data = "👍_Ferghana")],
+        [InlineKeyboardButton("Namangan", callback_data = "👍_Namangan"), InlineKeyboardButton("Sirdaryo", callback_data = "👍_Syr Darya")],
+        [InlineKeyboardButton("Smarqand", callback_data = "👍 Smarkand"), InlineKeyboardButton("Qashqadaryo", callback_data = "👍_Kashkadarya")],
+        [InlineKeyboardButton("Buxoro", callback_data="👍_Bukhra"), InlineKeyboardButton("Xorazim", callback_data = "👍_Khorazim")],
+        [InlineKeyboardButton("Navoiy", callback_data = "👍_Navoi"), InlineKeyboardButton("Jizzax", callback_data = "👍_Jizzakh")],
     ])
 
-    text = f"Assalomu alaykum {first_name} IslomNur botimizga hush kilibsiz \n Namoz vaqtlarini bilish uchun viloyatni tanlang"
+    text = f"Assalomu alaykum {first_name} IslomNur botimizga hush kilibsiz\n Namoz vaqtlarini bilish uchun viloyatni tanlang"
     update.message.reply_text(text,reply_markup=inlinekeyborad)
 
 def prayer(update:Update, contexr:CallbackContext):
     query = update.callback_query
     city = query.data
-    url = "https://aladhan.p.rapidapi.com/timingsByCity"
-
-    querystring = {"country":"Uzbekistan","city":f'{city}'}
-
-    headers = {
-        "X-RapidAPI-Key": "985cef3375msh3d3b11fac985565p1acdc4jsnbb0709f31f0b",
-        "X-RapidAPI-Host": "aladhan.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    data = response['data']['timings']
-    fajr = data["Fajr"]
-    sunrise = data["Sunrise"]
-    dhuhr = data["Dhuhr"]
-    asr = data["Asr"]
-    sunset = data["Sunset"]
-    maghrib = data["Maghrib"]
-    isha = data["Isha"]
-    imsak = data["Imsak"]
-    midnight = data["Midnight"]
-    firstthird = data["Firstthird"]
-    lastthird = data["Lastthird"]
+    print(city.split(" ")[-1])
+    print(prayer(city.split(" ")[-1]))
+    # text = f'bomdot:{fajr}'
+    # query.edit_message_text(text=text,parse_mode=ParseMode.HTML)
