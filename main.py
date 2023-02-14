c92bb820a4aa228b4f2bbfb8d1214b554d55be23
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-14 17:21:25 +0500

    texr to image and pillow

--- main.py
+++ main.py
@@ -1,42 +1,23 @@
 from PIL import Image, ImageDraw, ImageFont
 
 # TEXT TO IMAGE and pillow
-def text_to_image(text, text1, fontname, fontsize, colorText, ):
-    img = Image.open("image.jpg")
+def text_to_image(text, text1, text2, text3, text4, text5, text6, text7, text8,fontname, fontsize, colorText,):
+    img = Image.open("image.png")
     w, h = img.size
     d = ImageDraw.Draw(img)
     font = ImageFont.truetype(fontname ,fontsize)
     # image draw text size
-
+    d.text((w/6, h/4.3), text, fill=colorText, font=font)
+    d.text((w/3.5, h/3), text1, fill=colorText, font=font)
+    d.text((w/1.2, h/3), text2, fill=colorText, font=font)
+    d.text((w/3.7, h/2.5), text3, fill=colorText, font=font)
+    d.text((w/1.8, h/2.5), text4, fill=colorText, font=font)
+    d.text((w/3.7, h/2.2), text5, fill=colorText, font=font)
+    d.text((w/1.2, h/2.2), text6, fill=colorText, font=font)
+    d.text((w/3.7, h/1.9), text7, fill=colorText, font=font)
+    d.text((w/1.2, h/1.9), text8, fill=colorText, font=font)
     
     img.save("image1.png")
 
     
-print(text_to_image("", "Ubuntu-Medium.ttf", 30, (251,240,147),))
-
-# def getSize(txt, font):
-#     testImg = Image.new('RGB', (1, 1))
-#     testDraw = ImageDraw.Draw(testImg)
-#     return testDraw.textsize(txt, font)
-
-# if __name__ == '__main__':
-
-#     fontname = "arial.ttf"
-#     fontsize = 12  
-#     text = "example@gmail.com"
-    
-#     colorText = "black"
-#     colorOutline = "red"
-#     colorBackground = "white"
-
-
-#     font = ImageFont.truetype(fontname, fontsize)
-#     width, height = getSize(text, font)
-#     img = Image.new('RGB', (width+4, height+4), colorBackground)
-#     d = ImageDraw.Draw(img)
-#     d.text((2, height/2), text, fill=colorText, font=font)
-#     d.rectangle((0, 0, width+3, height+3), outline=colorOutline)
-    
-#     img.save("D:/image.png")
-
-# print(getSize("Salom Dunyo", "Arial.ttf"))
+# print(text_to_image("Ubuntu-Medium.ttf", 30, (251,240,147), "red", "white"))
