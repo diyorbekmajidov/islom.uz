303411a3a0691f22f1e2fd8ae1c4ac82869ca6b2
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-16 16:06:09 +0500

    image draw text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
 # TEXT TO IMAGE and pillow
-def text_to_image(text, text1,text3,text4,text5,text6,text7,text8,text9,text10,text11, fontname, fontsize, colorText, colorOutline, colorBackground):
+def text_to_image(text, text1,text2,text3,text4,text5,text6,text7,text8, fontname, fontsize, colorText, colorOutline, colorBackground):
     img = Image.open("img.jpg")
     w, h = img.size
     d = ImageDraw.Draw(img)
@@ -10,19 +10,18 @@ def text_to_image(text, text1,text3,text4,text5,text6,text7,text8,text9,text10,t
 
     d.text((w//2.5, h//6.3), text, fill=colorText, font=font, align="center")
     d.text((w//4.2, h//4.3), text1, fill=colorText, font=font, align="center")
-    d.text((w//13, h//3), text3, fill=colorText, font=font, align="center")
-    d.text((w//2.3, h//3), text4, fill=colorText, font=font, align="center")
-    d.text((w//13, h//2.5), text5, fill=colorText, font=font, align="center")
-    d.text((w//2.3, h//2.5), text6, fill=colorText, font=font, align="center")
-    d.text((w//13, h//2.2), text7, fill=colorText, font=font, align="center")
-    d.text((w//2.3, h//2.2), text8, fill=colorText, font=font, align="center")
-    d.text((w//13, h//1.9), text9, fill=colorText, font=font, align="center")
-    d.text((w//2.3, h//1.9), text10, fill=colorText, font=font, align="center")
-    d.text((w//13, h//1.2), text11, fill=colorText, font=font, align="center")
-    img.save("image.png")
+    d.text((w//5, h//2.8), text2, fill=colorText, font=font, align="center")
+    d.text((w//5, h//2.4), text3, fill=colorText, font=font, align="center")
+    d.text((w//5, h//2.1), text4, fill=colorText, font=font, align="center")
+    d.text((w//5, h//1.9), text5, fill=colorText, font=font, align="center")
+    d.text((w//5, h//1.7), text6, fill=colorText, font=font, align="center")
+    d.text((w//5, h//1.55), text7, fill=colorText, font=font, align="center")
+    d.text((w//13, h//1.2), text8, fill=colorText, font=font, align="center")
+    
+    img.save("ima.png")
 
     
-print(text_to_image("IslomNur","               vilayoti uchun \n namoz vaqtlari", "Bomdod   ","Quyosh chiqishi   ","Peshin   ","Asr   ","Shom   ","Quyosh botishi    ","Xufton    ","Yarim tun   ","Albatta Namoz mo'minlarga vaqtida \n farz qilingandir (Niso surasi. 103-oyat) ", "Ubuntu-Medium.ttf", 30, (251,240,147), "red", "white"))
+print(text_to_image("IslomNur","               vilayoti uchun \n namoz vaqtlari", "Bomdod   ","Quyosh   ","Peshin   ","Asr   ","Shom   ","Xufton    ","Albatta Namoz mo'minlarga vaqtida \n farz qilingandir (Niso surasi. 103-oyat) ", "Ubuntu-Medium.ttf", 30, (251,240,147), "red", "white"))
 
 # def getSize(txt, font):
 #     testImg = Image.new('RGB', (1, 1))
