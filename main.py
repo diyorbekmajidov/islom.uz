6c55fd4a9f49766f5cff81f8dfbe83ed81c9f1a4
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-17 15:28:54 +0500

    image draw text

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from PIL import Image, ImageDraw, ImageFont
 
 # TEXT TO IMAGE and pillow
-def text_to_image(text, text1,text2,text3,text4,text5, fontname, fontsize, colorText):
+def text_to_image(city,text, text1,text2,text3,text4,text5, fontname, fontsize, colorText):
     img = Image.open("ima.png")
     w, h = img.size
     d = ImageDraw.Draw(img)
     font = ImageFont.truetype(fontname ,fontsize)
     # image draw text size
-
+    d.text((w//4.3, h//4.3), city, fill=colorText, font=font, align="center")
     d.text((w//2, h//2.8), text, fill=colorText, font=font, align="center")
     d.text((w//2, h//2.4), text1, fill=colorText, font=font, align="center")
     d.text((w//2, h//2.1), text2, fill=colorText, font=font, align="center")
@@ -16,33 +16,3 @@ def text_to_image(text, text1,text2,text3,text4,text5, fontname, fontsize, color
     d.text((w//2, h//1.55), text5, fill=colorText, font=font, align="center")
     
     img.save("ima1.png")
-
-    
-# print(text_to_image("IslomNur","               vilayoti uchun \n namoz vaqtlari", "Bomdod   ","Quyosh   ","Peshin   ","Asr   ","Shom   ","Xufton    ","Albatta Namoz mo'minlarga vaqtida \n farz qilingandir (Niso surasi. 103-oyat) ", "Ubuntu-Medium.ttf", 30, (251,240,147), "red", "white"))
-
-# def getSize(txt, font):
-#     testImg = Image.new('RGB', (1, 1))
-#     testDraw = ImageDraw.Draw(testImg)
-#     return testDraw.textsize(txt, font)
-
-# if __name__ == '__main__':
-
-#     fontname = "arial.ttf"
-#     fontsize = 12  
-#     text = "example@gmail.com"
-    
-#     colorText = "black"
-#     colorOutline = "red"
-#     colorBackground = "white"
-
-
-#     font = ImageFont.truetype(fontname, fontsize)
-#     width, height = getSize(text, font)
-#     img = Image.new('RGB', (width+4, height+4), colorBackground)
-#     d = ImageDraw.Draw(img)
-#     d.text((2, height/2), text, fill=colorText, font=font)
-#     d.rectangle((0, 0, width+3, height+3), outline=colorOutline)
-    
-#     img.save("D:/image.png")
-
-# print(getSize("Salom Dunyo", "Arial.ttf"))
