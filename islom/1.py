d2ae3578699eb9be12ed03b00eb69aa3b8fe1c54
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-23 13:45:47 +0500

    parser sity

--- islom/1.py
+++ islom/1.py
@@ -21,26 +21,34 @@ def text_to_image(city,text, text1,text2,text3,text4,text5, fontname, fontsize, 
 
 # from PIL import Image, ImageDraw, ImageFont
 
-# # TEXT TO IMAGE and pillow
 # def text_to_image(text, text1,text2,text3,text4,text5,text6,text7,text8, fontname, fontsize, colorText, colorOutline, colorBackground):
-#     img = Image.open("img.jpg")
+#     img = Image.open("islom.jpg")
 #     w, h = img.size
 #     d = ImageDraw.Draw(img)
 #     font = ImageFont.truetype(fontname ,fontsize)
-    # image draw text size
-
-    # d.text((w//2.5, h//6.3), text, fill=colorText, font=font, align="center")
-#     d.text((w//4, h//4.3), text1, fill=colorText, font=font, align="center")
-#     d.text((w//4, h//2.8), text2, fill=colorText, font=font, align="center")
-#     d.text((w//4, h//2.4), text3, fill=colorText, font=font, align="center")
-#     d.text((w//4, h//2.1), text4, fill=colorText, font=font, align="center")
-#     d.text((w//4, h//1.9), text5, fill=colorText, font=font, align="center")
-#     d.text((w//4, h//1.7), text6, fill=colorText, font=font, align="center")
-#     d.text((w//4, h//1.55), text7, fill=colorText, font=font, align="center")
+#     # image draw text size
+#     # tumanlar
+#     d.text((w//2.5, h//6.3), text, fill=colorText, font=font, align="center") 
+#     # namoz vaqtlari
+#     d.text((w//7, h//3.8), text1, fill=colorText, font=font, align="center")
+#     # bomdod
+#     d.text((w//4, h//2.35), text2, fill=colorText, font=font, align="center")
+#     # quyosh
+#     d.text((w//4, h//2.05), text3, fill=colorText, font=font, align="center")
+#     # peshin
+#     d.text((w//4, h//1.8), text4, fill=colorText, font=font, align="center")
+#     # asr
+#     d.text((w//4, h//1.62), text5, fill=colorText, font=font, align="center")
+#     # shom
+#     d.text((w//4, h//1.48), text6, fill=colorText, font=font, align="center")
+#     # xufton
+#     d.text((w//4, h//1.35), text7, fill=colorText, font=font, align="center")
+#     # albatta namoz
 #     d.text((w//13, h//1.2), text8, fill=colorText, font=font, align="center")
     
 #     img.save("ima.png")
 
     
-# print(text_to_image("IslomNur","          vilayoti uchun \n namoz vaqtlari", "Bomdod   ","Quyosh   ","Peshin   ","Asr   ","Shom   ","Xufton    ","Albatta Namoz mo'minlarga vaqtida \n farz qilingandir (Niso surasi. 103-oyat) ", "Ubuntu-Medium.ttf", 30, (251,240,147), "red", "white"))
+# print(text_to_image("IslomNur","          \n vilayoti uchun namoz vaqtlari", "Bomdod   ","Quyosh   ","Peshin   ","Asr   ","Shom   ","Xufton    ","Albatta Namoz mo'minlarga vaqtida \n farz qilingandir (Niso surasi. 103-oyat) ", "Ubuntu-Medium.ttf", 30, (251,240,147), "red", "white"))
+
 
