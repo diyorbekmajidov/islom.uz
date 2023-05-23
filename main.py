2c1504f7ddce97d49ddf44b05d0b9fca92e09542
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-23 13:47:55 +0500

    add pillow center a city

--- main.py
+++ main.py
@@ -7,23 +7,21 @@ def text_to_image(city,text, text1,text2,text3,text4,text5, fontname, fontsize, 
     d = ImageDraw.Draw(img)
     font = ImageFont.truetype(fontname ,fontsize)
     # image draw text size
-    if city == "Qoraqalpog'iston":
-        d.text((w//8, h//4.3), city, fill=colorText, font=font, align="center")
-    elif city== "Samarqand" or city== "Namangan":
-        d.text((w//4, h//4.3), city, fill=colorText, font=font, align="center")
-    elif city== "Toshkent"  or city== "Sirdaryo" :
-        d.text((w//3.5, h//4.3), city, fill=colorText, font=font, align="center")
-    elif city== "Xorazim" or city=="Andijon" or city == "Farg'ona":
-        d.text((w//3.2, h//4.3), city, fill=colorText, font=font, align="center")
-    elif city== "Buxoro" or city== "Navoiy" or city == "Jizzax":
-        d.text((w//2.9, h//4.3), city, fill=colorText, font=font, align="center")
-    elif  city== "Surxondaryo" or city== "Qashqadaryo":
-        d.text((w//2.7, h//4.3), city, fill=colorText, font=font, align="center")
-    d.text((w//2, h//2.8), text, fill=colorText, font=font, align="center")
-    d.text((w//2, h//2.4), text1, fill=colorText, font=font, align="center")
-    d.text((w//2, h//2.1), text2, fill=colorText, font=font, align="center")
-    d.text((w//2, h//1.9), text3, fill=colorText, font=font, align="center")
-    d.text((w//2, h//1.7), text4, fill=colorText, font=font, align="center")
-    d.text((w//2, h//1.55), text5, fill=colorText, font=font, align="center")
+
+    image_width, image_height = img.size
+    text_width, text_height = d.textsize(city, font)
+    image_width, image_height = img.size
+    x = (image_width - text_width) // 2
+    y = (image_height - text_height) //4
+    
+    d.text((x,y), city, fill=colorText, font=font, align="center")
+    d.text((w//2, h//2.35), text, fill=colorText, font=font, align="center")
+    d.text((w//2, h//2.05), text1, fill=colorText, font=font, align="center")
+    d.text((w//2, h//1.8), text2, fill=colorText, font=font, align="center")
+    d.text((w//2, h//1.62), text3, fill=colorText, font=font, align="center")
+    d.text((w//2, h//1.48), text4, fill=colorText, font=font, align="center")
+    d.text((w//2, h//1.35), text5, fill=colorText, font=font, align="center")
     
     img.save("ima1.png")
+
+
